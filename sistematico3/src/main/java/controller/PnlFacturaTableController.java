ad32e57fd40eab0a32cb36df79598599a089eab6
uzielduarte <duarteuziel36@gmail.com>
2021-04-21 08:28:55 -0600

    Sistematico2

---
+++ sistematico3/src/main/java/controller/PnlFacturaTableController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.swing.table.DefaultTableModel;
+import panel.PnlFacturaTable;
+
+/**
+ *
+ * @author Sistemas-08
+ */
+public class PnlFacturaTableController {
+    private PnlFacturaTable pnlFacturaTable;
+    private DefaultTableModel tabla;
+    
+}
